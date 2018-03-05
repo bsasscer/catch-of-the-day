a1d9ed1fed3005964027f47a76400a94ade640eb
bsasscer <smithbi.maricela@gmail.com>
2018-03-05 11:37:40 -0800

    fix this console log error by adding binding to helper

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,13 +3,21 @@ import { getFunName } from '../helpers';
 
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = (event) => {
+        event.preventDefault();
+        console.log(this);
+    }
+
     render() {
         return (
             <Fragment>
-                <form className="store-selector">
+                <form className="store-selector" onSubmit={this.goToStore}>
                     <h2>Please Enter A Store</h2>
                     <input
                         type="text"
+                        ref={this.myInput}
                         required placeholder="Store Name"
                         defaultValue={getFunName()}
                     />
