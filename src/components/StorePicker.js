2c71c96a789d9899201c6253cdf83c5e3833b7ad
bsasscer <smithbi.maricela@gmail.com>
2018-03-05 11:18:10 -0800

    add more components and helper functions

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { getFunName } from '../helpers';
 
 
 class StorePicker extends React.Component {
@@ -7,7 +8,11 @@ class StorePicker extends React.Component {
             <Fragment>
                 <form className="store-selector">
                     <h2>Please Enter A Store</h2>
-                    <input type="text" required placeholder="Store Name" />
+                    <input
+                        type="text"
+                        required placeholder="Store Name"
+                        defaultValue={getFunName()}
+                    />
                     <button type="submit">Visit Store </button>
                 </form>
             </Fragment>
